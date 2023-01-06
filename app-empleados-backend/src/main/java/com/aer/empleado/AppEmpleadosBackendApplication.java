e09fbb051e9968bd7585c3b4b329873c33bb5f31
antonier-g <lianenra@gmail.com>
2023-01-06 18:59:04 -0500

    Proyecto inicial con acceso a la bdd Oracle y compilado sin errores

---
+++ app-empleados-backend/src/main/java/com/aer/empleado/AppEmpleadosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aer.empleado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEmpleadosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEmpleadosBackendApplication.class, args);
+	}
+
+}
